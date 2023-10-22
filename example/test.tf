c8809bd9a3bef7bf3451e36c7f9a55c7c5db358d
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2023-10-22 00:14:48 +0000

    Move provider configuration out of modules as that is considered a legacy way to do modules.

--- example/test.tf
+++ example/test.tf
@@ -1,3 +1,34 @@
+terraform {
+  required_version = "~> 1.6.0"
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.14"
+    }
+    ansible = {
+      source  = "ansible/ansible"
+      version = "1.1.0"
+    }
+  }
+}
+
+# See the argument reference documentation for how to configure the Proxmox
+# Provider for Terraform:
+# https://github.com/Telmate/terraform-provider-proxmox/blob/master/docs/index.md
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_api_token_id     = var.pm_api_token_id
+  pm_tls_insecure     = true
+  pm_log_enable       = true
+  pm_log_file         = "terraform-plugin-proxmox.log"
+  pm_debug            = true
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
+  }
+}
+
 # Only edit below if the module interface changed
 module "test" {
   source              = "git@github.com:dmbrownlee/proxmox-vyos-router"
--- main.tf
+++ main.tf
@@ -12,23 +12,6 @@ terraform {
   }
 }
 
-# See the argument reference documentation for how to configure the Proxmox
-# Provider for Terraform:
-# https://github.com/Telmate/terraform-provider-proxmox/blob/master/docs/index.md
-provider "proxmox" {
-  pm_api_url          = var.pm_api_url
-  pm_api_token_secret = var.pm_api_token_secret
-  pm_api_token_id     = var.pm_api_token_id
-  pm_tls_insecure     = true
-  pm_log_enable       = true
-  pm_log_file         = "terraform-plugin-proxmox.log"
-  pm_debug            = true
-  pm_log_levels = {
-    _default    = "debug"
-    _capturelog = ""
-  }
-}
-
 resource "proxmox_vm_qemu" "router" {
   name                   = var.hostname
   target_node            = var.target_node
